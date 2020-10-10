77e04374d71473b5b5377754fb56d3fb61ae54c0
Mark Richardson <BusinessGuy1979@gmail.com>
2020-10-10 17:54:44 -0500

    [CODE UPDATE] - Updated return variable.

--- 4. Experience/6/Hear/12/Other/3/Web Development/Experience/13/1_0/Experience_The_Hear_OfTheAPIServer_Message_12_3_1_0.cs
+++ 4. Experience/6/Hear/12/Other/3/Web Development/Experience/13/1_0/Experience_The_Hear_OfTheAPIServer_Message_12_3_1_0.cs
@@ -249,7 +249,7 @@ namespace BaseDI.BackEnd.Experience.Hear.Web_Development_13
 
             _request = _serverInfo.ContainsKey("Request") ? (HttpRequest)_serverInfo["Request"] : null;
 
-            if(_request != null)
+            if (_request != null)
             {
                 _route = _request.Path;
                 _verb = _request.Method;
@@ -282,7 +282,7 @@ namespace BaseDI.BackEnd.Experience.Hear.Web_Development_13
 
                 #endregion
 
-                if(_request != null && _request is HttpRequest)
+                if (_request != null && _request is HttpRequest)
                 {
                     #region 1. Assign
 
@@ -367,7 +367,7 @@ namespace BaseDI.BackEnd.Experience.Hear.Web_Development_13
 
                     Step_1_0_Custom_Control_ClientRequestToServer_1_0 = (JToken serverEnvironmentServerRoutes) =>
                     {
-                        if(serverEnvironmentServerRoutes != null)
+                        if (serverEnvironmentServerRoutes != null)
                         {
                             foreach (var item in serverEnvironmentServerRoutes)
                             {
@@ -377,17 +377,19 @@ namespace BaseDI.BackEnd.Experience.Hear.Web_Development_13
                                 var controllerModelDataLocalParameter = item.SelectToken("SetupItemTransportItemRoute.ModelDataLocalParameter").ToString();
                                 var controllerModelDataRemote = (JObject)item.SelectToken("SetupItemTransportItemRoute.ModelDataRemote");
 
-                                if(controllerRoutes != null && controllerRoutes.Count() > 0)
+                                if (controllerRoutes != null && controllerRoutes.Count() > 0)
                                 {
                                     var route = controllerRoutes.Where(r => r.ToString().ToUpper() == _route.ToUpper()).SingleOrDefault();
 
-                                    if(route != null)
+                                    if (route != null)
                                     {
                                         return Step_2_0_Custom_Output_ServerRequestToClient_1_0(route.ToString(), controllerName, controllerModelDataLocalObject, controllerModelDataLocalParameter, controllerModelDataRemote, _request, null);
-                                    }                                    
+                                    }
                                 }
                             }
                         }
+
+                        return _storylineDetails;
                     };
 
                     //READ ROUTES
@@ -399,8 +401,6 @@ namespace BaseDI.BackEnd.Experience.Hear.Web_Development_13
 
                     #region 3. Observe
 
-                    return _storylineDetails;
-
                     #endregion
                 }
             }
@@ -411,7 +411,6 @@ namespace BaseDI.BackEnd.Experience.Hear.Web_Development_13
 
             return await Task.FromResult<JObject>(_storylineDetails).ConfigureAwait(true);
         }
-
 
         #endregion
 
