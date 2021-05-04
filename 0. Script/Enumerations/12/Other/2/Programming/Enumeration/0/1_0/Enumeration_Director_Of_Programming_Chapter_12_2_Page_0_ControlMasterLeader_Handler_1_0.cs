50c6b4d2c72c2a2e999ac7ad63b7338ff3d6e6b6
Mark Richardson <BusinessGuy1979@gmail.com>
2021-05-04 12:18:50 -0500

    Task 18-10-1-1_PXXX_BDI_G12 - [Mark Richardson] - PROGRAMMING - OUTPUT plumbing enhancement - update code to better follow baseDI standards

--- 0. Script/Enumerations/12/Other/2/Programming/Enumeration/0/1_0/Enumeration_Director_Of_Programming_Chapter_12_2_Page_0_ControlMasterLeader_Handler_1_0.cs
+++ 0. Script/Enumerations/12/Other/2/Programming/Enumeration/0/1_0/Enumeration_Director_Of_Programming_Chapter_12_2_Page_0_ControlMasterLeader_Handler_1_0.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace BaseDI.Professional.Script.Programming.Enumeration_1
 {
-    public enum Enumeration_Studio_Automation_Programming_Chapter_12_2_Page_0_ControlMasterLeader_Handler_1_0
+    public enum Enumeration_Director_Of_Programming_Chapter_12_2_Page_0_ControlMasterLeader_Handler_1_0
     {
         RunAutoBackup = 1,
         RunMistake = 2
