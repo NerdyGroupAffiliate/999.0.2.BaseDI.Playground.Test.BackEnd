7f9e0cc93aebfbb316fcb86a3add71acbfb5dba2
Mark Richardson <BusinessGuy1979@gmail.com>
2021-05-13 17:59:23 -0500

    Task 18-10-1-1_PXXX_BDI_G12 - [Mark Richardson] - PROGRAMMING - OUTPUT plumbing enhancement - update code to better follow baseDI standards

--- 8. Testing/0. Script/Extension_Director_Of_WebDevelopment_Chapter_12_3_Page_0_GoalHelper_Handler_1_0.test.tsx
+++ 8. Testing/0. Script/Extension_Director_Of_WebDevelopment_Chapter_12_3_Page_0_GoalHelper_Handler_1_0.test.tsx
@@ -23,6 +23,12 @@ import * as LocalFile_Director_Of_Programming_Chapter_12_2_Page_2_ConversionRequ
 
 //#endregion
 
+//#region NodeJS
+
+const importedFileManager_VendorNodeJS = require('fs');
+
+//#endregion
+
 //#endregion
 
 //#region 1. Assign
@@ -99,7 +105,7 @@ beforeEach(() => {
         NODE_ENV: "development",
         PORT: 0,
         APP_ENV: "SERVER",
-        APP_SETTING_CONVERSION_MODE_12_3_WEBDEVELOPMENT_NICHE_MASTER:"LOCAL_FILE", //OPTIONS = LOCAL_FILE, LOCAL_DATABASE, LOCAL_SERVICE, REMOTE_FILE, REMOTE_DATABASE, REMOTE_SERVICE, REMOTE_SERVICE_VENDOR
+        APP_SETTING_CONVERSION_MODE_12_3_WEBDEVELOPMENT_NICHE_MASTER: "LOCAL_FILE", //OPTIONS = LOCAL_FILE, LOCAL_DATABASE, LOCAL_SERVICE, REMOTE_FILE, REMOTE_DATABASE, REMOTE_SERVICE, REMOTE_SERVICE_VENDOR
         APP_SETTING_DEVELOPER_MODE: true,
         APP_SETTING_DEVELOPER_MODE_SILENT: false
     }
@@ -113,7 +119,6 @@ beforeEach(() => {
 
     //#region MEMORIZE output xxx xxx
 
-
     //#endregion
 
     //#endregion
@@ -124,7 +129,7 @@ beforeEach(() => {
 
     //#region EXECUTE process defaults
 
-    //#region IDEAL CASE - defaults handler
+    //#region IDEAL CASE - USE defaults handler
 
 
     //#endregion
@@ -134,6 +139,15 @@ beforeEach(() => {
     //#endregion
 
     //#region 3. OUTPUT
+
+    //#region HANDLE execution response
+
+    //#region IDEAL CASE - USE baseDI data
+
+
+    //#endregion
+
+    //#endregion    
 
     //#endregion
 });
@@ -146,8 +160,129 @@ beforeEach(() => {
 
 //#region 4. Action
 
+const ExecuteConversionRequest = async (parameterInputs: SingleParmPoco_12_2_1_0.BaseDI.Professional.Script.Programming.Poco_1.SingleParmPoco_12_2_1_0): Promise<any> => {
+    //#region 1. INPUTS
+
+    //#region DEFINE control variables
+
+    //#endregion
+
+    //#region DEFINE input variables
+
+
+    //#endregion
+
+    //#region DEFINE process variables
+
+
+    //#endregion
+
+    //#region DEFINE output variables
+
+    let storedOutputResponseData: any = null;
+    let storedOutputResponseProcess: any = null;
+
+    //#endregion
+
+    ///////////////////////////////////////////////////////////
+
+    //#region MEMORIZE control variables
+
+    //#region MEMORIZE control xxx xxx
+
+    //#endregion
+
+    //#endregion
+
+    //#region MEMORIZE input variables
+
+    //#region MEMORIZE input xxx xxxx
+
+
+    //#endregion
+
+    //#endregion
+
+    //#region MEMORIZE process variables
+
+    //#region MEMORIZE process test variables
+
+    let storedInputs = new SingleParmPoco_12_2_1_0.BaseDI.Professional.Script.Programming.Poco_1.SingleParmPoco_12_2_1_0();
+
+    //0. CONTROLLERS
+
+    //1. INPUTS
+    storedInputs.Parameters.setValue("parameterInputRequestActionName", Action_12_2_1_0.BaseDI.Professional.Script.Programming.Poco_1.Action_12_2_1_0._12_3_WEB_DEVELOPMENT_Server_Process_HTTP_Request_1_0);
+    storedInputs.Parameters.setValue("parameterInputRequestName", parameterInputs.Parameters.getValue("parameterInputRequestName"));
+    storedInputs.Parameters.setValue("parameterInputRequestNameDataCacheKey", parameterInputs.Parameters.getValue("parameterInputRequestNameDataCacheKey"));
+
+    //2. PROCESS
+    storedInputs.Parameters.setValue("parameterProcessRequestTracker", _storedProcessRequestTracker);
+    storedInputs.Parameters.setValue("parameterProcessRequestSettings", _storedProcessRequestTracker["storedProcessRequestSettings"]);
+    storedInputs.Parameters.setValue("parameterProcessRequestRequestHandlerFileName", parameterInputs.Parameters.getValue("parameterInputRequestName"));
+
+    storedInputs.Parameters.setValue("parameterProcessRequestExtraData", null);
+
+    _storedProcessRequestCentralizedStorer = new Implement_DesignPattern_Factory_Storer_12_2_1_0.BaseDI.Professional.Story.Programming_0.Implement_DesignPattern_Factory_Storer_12_2_1_0(storedInputs);
+
+    storedInputs.Parameters.setValue("parameterProcessRequestCentralizedDisturber", null);
+    storedInputs.Parameters.setValue("parameterProcessRequestCentralizedSensor", null);
+    storedInputs.Parameters.setValue("parameterProcessRequestCentralizedStorer", _storedProcessRequestCentralizedStorer);
+
+    _storedProcessRequestLocalCacheDataRepository = new ChapterPage_Page_2_1_Begin_Process_12_2_1_0.BaseDI.Professional.Chapter.Page.Programming_2.Page_2_1_Begin_Process_12_2_1_0(storedInputs);
+    _storedProcessRequestLocalCacheDataRepository.Repository = new LocalFile_Director_Of_Programming_Chapter_12_2_Page_2_ConversionRequest_Handler_1_0.BaseDI.Professional.State.Programming_2.LocalFile_Director_Of_Programming_Chapter_12_2_Page_2_ConversionRequest_Handler_1_0(storedInputs);
+
+    //3. OUTPUT
+    storedOutputResponseProcess = await Promise.all([_storedProcessRequestLocalCacheDataRepository.Action()]);
+    storedInputs.Parameters.setValue("parameterProcessRequestDataStorylineDetails", storedOutputResponseProcess[0].StorylineDetails);
+
+    //3. OUTPUTS
+
+    //#endregion
+
+    //#endregion
+
+    //#region MEMORIZE output variables
+
+    //#region MEMORIZE output xxx xxxx
+
+    //#endregion
+
+    //#endregion
+
+    //#endregion
+
+    //#region 2. PROCESS
+
+    //#region HANDLE application defaults
+
+    //#region IDEAL CASE - USE baseDI converter
+
+    storedOutputResponseData = await Extension_Director_Of_WebDevelopment_Chapter_12_3_Page_0_GoalHelper_Handler_1_0.BaseDI.Professional.Web_Development.Extensions_0.Extension_Director_Of_WebDevelopment_Chapter_12_3_Page_0_GoalHelper_Handler_1_0.Step_0_0_Framework_Convert_HtmlToJSON_1_0(storedInputs);
+
+    //#endregion
+
+    //#endregion
+
+    //#endregion
+
+    //#region 3. OUTPUT
+
+    //#region HANDLE execution response
+
+    //#region IDEAL CASE - USE baseDI data
+
+    return storedOutputResponseData;
+
+    //#endregion
+
+    //#endregion    
+
+    //#endregion
+}
+
 describe('Web Development Test', () => {
-    test('Conversion Test - Convert JSON to HTML', async () =>
+    test('1. Homepage - Conversion Test - Convert JSON to HTML', async () =>
     {
         //#region 1. INPUTS
 
@@ -163,19 +298,21 @@ describe('Web Development Test', () => {
         //#region DEFINE process variables
 
 
+
         //#endregion
 
         //#region DEFINE output variables
 
-        let storedOutputResponseProcess: any = null;
+        let storedOutputResponseData: any = null;
 
         //#endregion
 
-        ///////////////////////////////////////////////////////////
+        ///////////////////////////////
 
         //#region MEMORIZE control variables
 
         //#region MEMORIZE control xxx xxx
+
 
         //#endregion
 
@@ -194,36 +331,10 @@ describe('Web Development Test', () => {
 
         //#region MEMORIZE process test variables
 
-        _storedInputs = new SingleParmPoco_12_2_1_0.BaseDI.Professional.Script.Programming.Poco_1.SingleParmPoco_12_2_1_0();
+        let storedInputs = new SingleParmPoco_12_2_1_0.BaseDI.Professional.Script.Programming.Poco_1.SingleParmPoco_12_2_1_0();
 
-        //0. CONTROLLERS
-
-        //1. INPUTS
-        _storedInputs.Parameters.setValue("parameterInputRequestActionName", Action_12_2_1_0.BaseDI.Professional.Script.Programming.Poco_1.Action_12_2_1_0._12_3_WEB_DEVELOPMENT_Server_Process_HTTP_Request_1_0);
-        _storedInputs.Parameters.setValue("parameterInputRequestName", "Director_Of_WebDevelopment_Chapter_12_3_Page_1_OutputHomeScreen_Handler_1_0");
-        _storedInputs.Parameters.setValue("parameterInputRequestNameDataCacheKey", "Director_Of_WebDevelopment_Chapter_12_3_Page_1_OutputHomeScreen_Handler_1_0-P1_0");
-
-        //2. PROCESS
-        _storedInputs.Parameters.setValue("parameterProcessRequestTracker", _storedProcessRequestTracker);
-        _storedInputs.Parameters.setValue("parameterProcessRequestSettings", _storedProcessRequestTracker["storedProcessRequestSettings"]);
-        _storedInputs.Parameters.setValue("parameterProcessRequestRequestHandlerFileName", "Director_Of_WebDevelopment_Chapter_12_3_Page_1_OutputHomeScreen_Handler_1_0");
-
-        _storedInputs.Parameters.setValue("parameterProcessRequestExtraData", null);
-
-        _storedProcessRequestCentralizedStorer = new Implement_DesignPattern_Factory_Storer_12_2_1_0.BaseDI.Professional.Story.Programming_0.Implement_DesignPattern_Factory_Storer_12_2_1_0(_storedInputs);
-
-        _storedInputs.Parameters.setValue("parameterProcessRequestCentralizedDisturber", null);
-        _storedInputs.Parameters.setValue("parameterProcessRequestCentralizedSensor", null);
-        _storedInputs.Parameters.setValue("parameterProcessRequestCentralizedStorer", _storedProcessRequestCentralizedStorer);
-
-        _storedProcessRequestLocalCacheDataRepository = new ChapterPage_Page_2_1_Begin_Process_12_2_1_0.BaseDI.Professional.Chapter.Page.Programming_2.Page_2_1_Begin_Process_12_2_1_0(_storedInputs);
-        _storedProcessRequestLocalCacheDataRepository.Repository = new LocalFile_Director_Of_Programming_Chapter_12_2_Page_2_ConversionRequest_Handler_1_0.BaseDI.Professional.State.Programming_2.LocalFile_Director_Of_Programming_Chapter_12_2_Page_2_ConversionRequest_Handler_1_0(_storedInputs);
-
-        //3. OUTPUT
-        storedOutputResponseProcess = await Promise.all([_storedProcessRequestLocalCacheDataRepository.Action()]);
-        _storedInputs.Parameters.setValue("parameterProcessRequestDataStorylineDetails", storedOutputResponseProcess[0].StorylineDetails);
-
-        //3. OUTPUTS
+        storedInputs.Parameters.setValue("parameterInputRequestName", "Director_Of_WebDevelopment_Chapter_12_3_Page_1_OutputHomeScreen_Handler_1_0");
+        storedInputs.Parameters.setValue("parameterInputRequestNameDataCacheKey", "Director_Of_WebDevelopment_Chapter_12_3_Page_1_OutputHomeScreen_Handler_1_0-P1_0")
 
         //#endregion
 
@@ -231,7 +342,8 @@ describe('Web Development Test', () => {
 
         //#region MEMORIZE output variables
 
-        //#region MEMORIZE output xxx xxxx
+        //#region MEMORIZE output xxx xxx
+
 
         //#endregion
 
@@ -241,11 +353,11 @@ describe('Web Development Test', () => {
 
         //#region 2. PROCESS
 
-        //#region HANDLE application defaults
+        //#region EXECUTE process defaults
 
-        //#region IDEAL CASE - USE baseDI converter
+        //#region IDEAL CASE - USE baseDI extension
 
-        _storedOutputResponseData = await Extension_Director_Of_WebDevelopment_Chapter_12_3_Page_0_GoalHelper_Handler_1_0.BaseDI.Professional.Web_Development.Extensions_0.Extension_Director_Of_WebDevelopment_Chapter_12_3_Page_0_GoalHelper_Handler_1_0.Step_0_0_Framework_Convert_HtmlToJSON_1_0(_storedInputs);
+        storedOutputResponseData = await ExecuteConversionRequest(storedInputs);
 
         //#endregion
 
@@ -257,9 +369,20 @@ describe('Web Development Test', () => {
 
         //#region HANDLE execution response
 
-        console.log(_storedOutputResponseData);
+        //#region IDEAL CASE - USE baseDI data
+
+        try
+        {
+            //importedFileManager_VendorNodeJS.writeFileSync(".\8. Testing\8. Testing\0. Script\Results\Extensions\12\3\Web Development\Report\0\1_0\" + storedInputs.Parameters.getValue("parameterInputRequestNameDataCacheKey") + ".json", JSON.stringify(storedOutputResponseData));
+            console.log(JSON.stringify(storedOutputResponseData));
+        }
+        catch (storedProcessRequestMistake) {
+            console.log(storedProcessRequestMistake.message);
+        }        
 
         //#endregion
+
+        //#endregion    
 
         //#endregion
     })
