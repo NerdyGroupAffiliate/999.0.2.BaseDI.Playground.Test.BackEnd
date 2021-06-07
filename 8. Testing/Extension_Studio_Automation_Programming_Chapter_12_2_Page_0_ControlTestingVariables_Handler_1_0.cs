1cb07c3ed73b32a1c276587648d2183136d6cc2f
Mark Richardson <BusinessGuy1979@gmail.com>
2021-06-07 06:17:24 -0500

    Task 18-10-1-1_PXXX_BDI_G12 - [Mark Richardson] - PROGRAMMING - OUTPUT plumbing enhancement - update code to better follow baseDI standards

---
+++ 8. Testing/Extension_Studio_Automation_Programming_Chapter_12_2_Page_0_ControlTestingVariables_Handler_1_0.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaseDI.Professional.Test
+{
+    [TestClass]
+    public class Extension_Studio_Automation_Programming_Chapter_12_2_Page_0_ControlTestingVariables_Handler_1_0
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string a = "";
+        }
+    }
+}
